b67aecdc8de1f32866e0995a2ad0e78efb35a6f9
Armen <armeniki@Hotmail.com>
2023-01-21 17:17:32 -0800

    Update TeleCardEmulator.ino
    I have abandoned the use of an ISR for sending card bits and have decided to use a simple flag process in the main loop.  These cards only run at about 15kHz in any event so it's not a big deal.  The next step is to clean up and do some maths to get the bits sent out correctly.

--- TeleCardEmulator.ino
+++ TeleCardEmulator.ino
@@ -1,5 +1,5 @@
 // QuickChange/TeleCard Emulator
-// v0.6
+// v0.7
 // armeniki
 // https://github.com/armeniki/TeleCardEmulator/
 // 15 Jan 2023
@@ -21,42 +21,56 @@ bool status_Reset = 0; //Card's ATR/Reset status
 bool flag_Reset = 0; //Used to complete Reset sequence
 bool line_CLK;  //Data from reading Clock pin
 bool line_RESET; //Data from reading Reset pin
-
+bool trigger;
 void setup()
 {
 	Serial.begin(115200);  //Open serial port
 	Serial.print("TeleCard Emulator - Started\n"); //Indicate sketch is running
-
 	pinMode(pin_CLK, INPUT_PULLUP); //Set pin modes
 	pinMode(pin_RESET, INPUT_PULLUP); // "  "
 	pinMode(pin_DATA, OUTPUT); // "  "
-	digitalWrite(pin_DATA, 0); //Start with DATA line low
-	attachInterrupt(digitalPinToInterrupt(pin_CLK), sendBits, RISING);  //Used to run ISR
+	digitalWrite(pin_DATA, 1); //Data line stays high normally
 }
 
 void loop()
 {
-	
 	line_CLK = digitalRead(pin_CLK);  //Keep logical level in variable to avoid reading multiple times
 	line_RESET = digitalRead(pin_RESET); // "             "             "             "             "
 
-	//This is the very basic card reset routine
-	if (line_RESET == 1){
-		if (line_CLK == 1){
-			flag_Reset = 1;
-		}
-		if (line_CLK == 0 && flag_Reset == 1){
-			resetCard();
-		} //Once the flag is set and CLOCK goes low, call the resetCard function
+
+	//Check if reset is requested
+	if (line_RESET == 1 && line_CLK == 0){
+		digitalWrite(pin_DATA, 0); //DATA is always low when RESET is high
+		flag_Reset = 1; //We set a flag to clear
 	}
+	if (line_RESET == 0 && line_CLK == 0 && flag_Reset){
+		flag_Reset = 0;
+		resetCard();
+	}
+	
+	
+	if (line_CLK == 1)
+	{
+		trigger = 1;
+	}
+
+	if (line_CLK == 0 && trigger){
+	
+		sendBits();
+		trigger = 0;
+	}
+
+
+
 }
 
 
 void resetCard(){
 	Serial.println("Reset Called");
+	digitalWrite(pin_DATA, 1);
 	flag_Reset = 0;
 	indexByte = 0;
-	indexBit = 1;
+	indexBit = 1;  //Set to 0 during TEST1 sample, and set to 1 during TEST2 sample
 	outputByte = 0;
 	outputBit = 0;
 	status_Reset = 1;
@@ -65,38 +79,34 @@ void resetCard(){
 
 void sendBits()
 {
-		outputByte = (cardbytes[indexByte]);		
-		outputBit = bitRead(outputByte, indexBit); //MSB sent out first
-		
-			
-			if (outputBit == 1){
-				
-					
-				PORTD |= _BV(PB5); //write port HIGH
-			}
-			else
-			{
-				
-				
-				PORTD &= ~_BV(PB5); //write port LOW
-			}
-		
+	
 
-		indexBit++;  //Increment the bit index for the next use by the bitRead function
-		
-		
+	outputByte = (cardbytes[indexByte]);
+	outputBit = bitRead(outputByte, indexBit); //MSB sent out first
 
-		if (indexBit == 8){  //Once all 8 bits have been read, time to go to the next byte in the array and reset the bit index
-			delayMicroseconds(10);
-			indexByte = indexByte + 1;
-			indexBit = 0;
-		}
 
-		if (indexByte == 16){ //Once all 16 bytes have been read, set the index back to 0
-			
-			indexByte = 0;
-			
-		}
-		
+	if (outputBit == 1){PORTD |= _BV(PB5); //write port HIGH
 	}
+	if (outputBit == 0){PORTD &= ~_BV(PB5); //write port LOW
+	}
+
+
+	
+
+
+
+	if (indexBit == 7){  //Once all 8 bits have been read, time to go to the next byte in the array and reset the bit index
+		
+		indexByte = indexByte + 1;
+		indexBit = 0;
+	}
+
+	if (indexByte == 15){ //Once all 16 bytes have been read, set the index back to 0
+		indexByte = 0;
+
+	}
+
+	indexBit++;  //Increment the bit index for the next use by the bitRead function
+
+}
 
