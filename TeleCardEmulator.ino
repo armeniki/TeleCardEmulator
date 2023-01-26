7174332176357c6101a23efdf3463b12f3d22de3
Armen <armeniki@Hotmail.com>
2023-01-26 08:30:11 -0800

    Update TeleCardEmulator.ino
    Re-instated ISR due to previous clock and data lag.

--- TeleCardEmulator.ino
+++ TeleCardEmulator.ino
@@ -1,18 +1,19 @@
 // QuickChange/TeleCard Emulator
-// v1.0
+// v1.1
 // (C) 2023 armeniki
 // https://github.com/armeniki/TeleCardEmulator/
-// 15 Jan 2023
+// 25 Jan 2023
 // Tested on an Arduino Pro Mini 5V 16MHz
 
-#define DEBUG
+//#define DEBUG  //Running with DEBUG may adversely affect data speeds
 
 #define pin_RESET     3   
 #define pin_CLK       2   
 #define pin_DATA      5 
 
 //This array is populated with a 16 byte QuickChange or TeleCard number.
-byte cardbytes[16] = { 0x99, 0x28, 0x1c, 0x02, 0xa1, 0xb3, 0x8d, 0xf4, 0x00, 0x00, 0x11, 0x77, 0x0f, 0xff, 0xff, 0xff }; //test card data
+//NB: Your terminal may or may not recognise this card number and depends on the entries in your terminal's card tables.
+byte cardbytes[16] = { 0x99, 0x28, 0x1c, 0x02, 0xa1, 0xb3, 0x8d, 0xf4, 0x00, 0x22, 0x11, 0x77, 0x0f, 0xff, 0xff, 0xff };
 byte cardbits[128];
 
 byte indexByte = 0;  //To keep track of current byte being read
@@ -20,8 +21,8 @@ uint8_t indexBit = 0; //To keep track of current bit being read
 bool flag_Reset = 0; //Used to complete Reset sequence
 bool line_CLK;  //Data from reading Clock pin
 bool line_RESET; //Data from reading Reset pin
-bool trigger;
 uint8_t addressBit = 0;
+
 
 void setup()
 {
@@ -33,7 +34,9 @@ void setup()
 	pinMode(pin_RESET, INPUT_PULLUP); // "  "
 	pinMode(pin_DATA, OUTPUT); // "  "
 	digitalWrite(pin_DATA, 1); //Data line stays high normally
+	attachInterrupt(digitalPinToInterrupt(2), sendBit, FALLING);
 	cardBytesToBits();
+
 
 }
 void loop()
@@ -41,7 +44,8 @@ void loop()
 	line_CLK = digitalRead(pin_CLK);  //Keep logical level in variable to avoid reading multiple times
 	line_RESET = digitalRead(pin_RESET); // "             "             "             "             "
 
-	if (line_RESET == 1){ digitalWrite(pin_DATA, 0); } //DATA line should always be low when RESET line is high
+	if (line_RESET == 1){ 
+		digitalWrite(pin_DATA, 0);	} //DATA line should always be low when RESET line is high
 
 	//Check if reset is requested
 	if (line_RESET == 1 && line_CLK == 0){
@@ -53,37 +57,27 @@ void loop()
 		resetCard();
 	}
 	
-	//Send out bits as required upon CLOCK going low
-	if (line_CLK == 1 && !flag_Reset && !trigger)
-	{
-		increment_address();
-		trigger = 1;
-	}
+	
+}
 
-	if (line_CLK == 0 && trigger){
-		sendBit();
-		trigger = 0;
-	}
+void sendBit(){
+	
+	if (line_RESET == 0){ //DATA does not go out during RESET high
+		addressBit++;
+		digitalWrite(5, cardbits[addressBit]);	}
 }
-void increment_address(){
-	addressBit++;
-}
-	void sendBit(){
-		if (cardbits[addressBit] == 1){
-		PORTD |= _BV(PB5); //write port HIGH
-	}
-		if (cardbits[addressBit] == 0){
-		PORTD &= ~_BV(PB5); //write port LOW
-	}
 
-}
-	void resetCard(){
+	
+void resetCard(){
+#ifdef DEBUG
 		Serial.println("Reset Called");
+#endif
 		digitalWrite(pin_DATA, 1);
 		flag_Reset = 0;
 		indexByte = 0;
 		indexBit = 0;
 		addressBit = 0;
+		
 	}
 void cardBytesToBits(){
 	
@@ -99,7 +93,7 @@ void cardBytesToBits(){
 		cardbits[i] = bitRead(cardbytes[c], indexBit);
 		
 #ifdef DEBUG
-		snprintf(string, sizeof(string), "Card byte: 0x%02X, Index bit: %u, Counter: %u, BIT STATUS: %u\n", cardbytes[c], indexBit, i, cardbits[i]);
+		snprintf(string, sizeof(string), "Card byte: 0x%02X, Index bit: %u, Counter: %u, Bit Setting: %u\n", cardbytes[c], indexBit, i, cardbits[i]);
 		Serial.print(string);
 #endif
 		
@@ -108,14 +102,9 @@ void cardBytesToBits(){
 			indexBit = 8;
 			c++;
 		}
-
 		indexBit--;
 		
 
 	}
-
-
-
-
 
 	}
