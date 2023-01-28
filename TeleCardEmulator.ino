e3af04dfa7de2f8ce34133b9bdbe5f93851e02e0
Armen <armeniki@Hotmail.com>
2023-01-27 16:28:00 -0800

    Update TeleCardEmulator.ino
    Replaced "digitalWrites" with direct PORT commands for faster response times and some other cleanup.

--- TeleCardEmulator.ino
+++ TeleCardEmulator.ino
@@ -13,12 +13,12 @@
 
 //This array is populated with a 16 byte QuickChange or TeleCard number.
 //NB: Your terminal may or may not recognise this card number and depends on the entries in your terminal's card tables.
-byte cardbytes[16] = { 0x99, 0x28, 0x1c, 0x02, 0xa1, 0xb3, 0x8d, 0xf4, 0x00, 0x22, 0x11, 0x77, 0x0f, 0xff, 0xff, 0xff };
+byte cardbytes[16] = { 0x99, 0x28, 0x1c, 0x02, 0xa1, 0xb3, 0x8d, 0xf4, 0x00, 0x00, 0x11, 0x77, 0x0f, 0xff, 0xff, 0xff };
 byte cardbits[128];
 
 byte indexByte = 0;  //To keep track of current byte being read
 uint8_t indexBit = 0; //To keep track of current bit being read
-bool flag_Reset = 0; //Used to complete Reset sequence
+bool flagReset = 0; //Used to complete Reset sequence
 bool line_CLK;  //Data from reading Clock pin
 bool line_RESET; //Data from reading Reset pin
 uint8_t addressBit = 0;
@@ -34,77 +34,87 @@ void setup()
 	pinMode(pin_RESET, INPUT_PULLUP); // "  "
 	pinMode(pin_DATA, OUTPUT); // "  "
 	digitalWrite(pin_DATA, 1); //Data line stays high normally
-	attachInterrupt(digitalPinToInterrupt(2), sendBit, FALLING);
+	attachInterrupt(digitalPinToInterrupt(2), processBit, FALLING);
 	cardBytesToBits();
 
 
 }
 void loop()
 {
-	line_CLK = digitalRead(pin_CLK);  //Keep logical level in variable to avoid reading multiple times
-	line_RESET = digitalRead(pin_RESET); // "             "             "             "             "
 
-	if (line_RESET == 1){ 
-		digitalWrite(pin_DATA, 0);	} //DATA line should always be low when RESET line is high
+	line_CLK = bitRead(PIND, PIND2);
+	line_RESET = bitRead(PIND, PIND3);
 
 	//Check if reset is requested
 	if (line_RESET == 1 && line_CLK == 0){
-		digitalWrite(pin_DATA, 0); //DATA is always low when RESET is high
-		flag_Reset = 1; //We set a flag to clear
+		flagReset = 1; //We set a flag to clear
 	}
-	if (line_RESET == 0 && line_CLK == 0 && flag_Reset){
-		flag_Reset = 0;
+	if (line_RESET == 0 && line_CLK == 0 && flagReset){
+		flagReset = 0;
 		resetCard();
 	}
-	
-	
+
+
 }
 
-void sendBit(){
-	
+void processBit(){
+
 	if (line_RESET == 0){ //DATA does not go out during RESET high
 		addressBit++;
-		digitalWrite(5, cardbits[addressBit]);	}
+		if (cardbits[addressBit] == 1){
+			PORTD |= _BV(PB5); //write port HIGH
+		}
+		if (cardbits[addressBit] == 0){
+			PORTD &= ~_BV(PB5); //write port LOW
+
+
+		}
+	}
+
+	if (line_RESET == 1){
+		cardbits[addressBit] = digitalRead(pin_DATA);
+	
+	}
+
 }
 
-	
 void resetCard(){
 #ifdef DEBUG
-		Serial.println("Reset Called");
+	Serial.println("Reset Called");
 #endif
-		digitalWrite(pin_DATA, 1);
-		flag_Reset = 0;
-		indexByte = 0;
-		indexBit = 0;
-		addressBit = 0;
-		
-	}
+	digitalWrite(pin_DATA, 1);
+	flagReset = 0;
+	indexByte = 0;
+	indexBit = 0;
+	addressBit = 0;
+
+}
 void cardBytesToBits(){
-	
+
 	//Store card bytes into bit array for easy management
 	char string[100];
 	indexBit = 7;
 	int c = 0;
 	int i = 0;
 
-	for (i = 0;  i < 128; i++)
+	for (i = 0; i < 128; i++)
 	{
-		
+
 		cardbits[i] = bitRead(cardbytes[c], indexBit);
-		
+
 #ifdef DEBUG
 		snprintf(string, sizeof(string), "Card byte: 0x%02X, Index bit: %u, Counter: %u, Bit Setting: %u\n", cardbytes[c], indexBit, i, cardbits[i]);
 		Serial.print(string);
 #endif
-		
+
 
 		if (indexBit == 0x00){
 			indexBit = 8;
 			c++;
 		}
 		indexBit--;
-		
+
 
 	}
 
-	}
+}
